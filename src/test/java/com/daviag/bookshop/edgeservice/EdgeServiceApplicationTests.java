f77bac702ebe6a932e1cc205bfd017a374053a63
David V. <villarejod@gmail.com>
2024-10-04 11:05:54 +0200

    fix tests

--- src/test/java/com/daviag/bookshop/edgeservice/EdgeServiceApplicationTests.java
+++ src/test/java/com/daviag/bookshop/edgeservice/EdgeServiceApplicationTests.java
@@ -2,11 +2,16 @@ package com.daviag.bookshop.edgeservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class EdgeServiceApplicationTests {
+
+	@MockBean
+	ReactiveClientRegistrationRepository clientRegistrationRepository;
 
 	@Test
 	void contextLoads() {
